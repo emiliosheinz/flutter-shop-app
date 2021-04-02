784a290e1d5be42abc2a01094cc0be135c4bf801
emilioheinz <emiliosheinz@gmail.com>
2021-04-02 20:33:35 -0300

    feat: future builder

--- lib/screens/orders.screen.dart
+++ lib/screens/orders.screen.dart
@@ -4,56 +4,42 @@ import 'package:shop_app/components/main_drawer.component.dart';
 import 'package:shop_app/components/order_item.component.dart';
 import 'package:shop_app/providers/orders.provider.dart' show Orders;
 
-class OrdersScreen extends StatefulWidget {
-  static const String routeName = '/orders';
-
-  @override
-  _OrdersScreenState createState() => _OrdersScreenState();
-}
-
-class _OrdersScreenState extends State<OrdersScreen> {
-  var _isInit = true;
-  var _isLoading = false;
-
-  @override
-  void didChangeDependencies() {
-    if (_isInit) {
-      setState(() {
-        _isLoading = true;
-      });
-
-      Provider.of<Orders>(context, listen: false).fetchAndSetOrders().then((_) {
-        setState(() {
-          _isLoading = false;
-        });
-      });
-    }
-
-    _isInit = false;
-    super.didChangeDependencies();
-  }
-
+class OrdersScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final ordersProvider = Provider.of<Orders>(context);
-
     return Scaffold(
       drawer: MainDrawer(),
       appBar: AppBar(
         title: Text('Your Orders'),
       ),
-      body: _isLoading
-          ? Center(child: CircularProgressIndicator())
-          : RefreshIndicator(
-              onRefresh: () => Provider.of<Orders>(context, listen: false)
-                  .fetchAndSetOrders(),
-              child: ListView.builder(
-                itemCount: ordersProvider.orders.length,
-                itemBuilder: (context, i) => OrderItem(
-                  ordersProvider.orders[i],
-                ),
-              ),
-            ),
+      body: RefreshIndicator(
+        onRefresh: () =>
+            Provider.of<Orders>(context, listen: false).fetchAndSetOrders(),
+        child: FutureBuilder(
+          future:
+              Provider.of<Orders>(context, listen: false).fetchAndSetOrders(),
+          builder: (context, dataSnapshot) {
+            if (dataSnapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            } else {
+              if (dataSnapshot.error != null) {
+                return Center(child: Text("An error ocurred!"));
+              } else {
+                return Consumer<Orders>(
+                  builder: (_, orderData, child) => ListView.builder(
+                    itemCount: orderData.orders.length,
+                    itemBuilder: (context, i) => OrderItem(
+                      orderData.orders[i],
+                    ),
+                  ),
+                );
+              }
+            }
+          },
+        ),
+      ),
     );
   }
 }
